2e5675a09005cb7c2b758d2052f0653c1572f7b5
Waltteri Heino <wheino@c3r1p7.hive.fi>
2025-05-14 14:01:09 +0300

    Moved necessary functions from libft to the testing main.c as the use of libft is not allowed in this project

--- Testing/main.c
+++ Testing/main.c
@@ -6,14 +6,99 @@
 /*   By: wheino <wheino@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 13:00:36 by wheino            #+#    #+#             */
-/*   Updated: 2025/05/13 15:26:26 by wheino           ###   ########.fr       */
+/*   Updated: 2025/05/14 13:59:21 by wheino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include "libft.h"
+#include <stdlib.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char		*dest_str;
+	const unsigned char	*src_str;
+	size_t				i;
+
+	dest_str = (unsigned char *)dest;
+	src_str = (const unsigned char *)src;
+	i = 0;
+	while (n--)
+	{
+		dest_str[i] = src_str[i];
+		i++;
+	}
+	return (dest_str);
+}
+
+size_t	ft_strlen(const char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if ((unsigned char)s[i] == (unsigned char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == c)
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		len;
+	char	*str;
+	int		i;
+	int		j;
+
+	len = ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1;
+	str = malloc(len);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		j++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+
+char	*ft_strdup(const char *s)
+{
+	char	*new_str;
+	size_t	len;
+
+	len = ft_strlen((char *)s) + 1;
+	new_str = malloc(len * sizeof(char));
+	if (!new_str)
+		return (NULL);
+	ft_memcpy(new_str, s, len);
+	return (new_str);
+}
 
 char	*ft_strldup(const char *s, size_t len)
 {
@@ -46,11 +131,11 @@ char *get_next_line(int fd)
 {
 	char		*buf;
 	static char	*stash;
-	static char	*temp_stash;
+	char	*temp_stash;
 	char		*line;
 	int			bytes_read;
 
-	buf = malloc(5);
+	buf = malloc(5 + 1);
 	if (stash == NULL)
 		stash = ft_strdup("");
 	while (1)
